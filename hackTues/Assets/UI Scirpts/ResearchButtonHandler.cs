3b06e2e1051f54dcbb7b550ef19aec9afc7a2c08
AlexandarMilissov <alex.milissov@gmail.com>
2019-03-15 18:04:09 +0200

    added funtions to the research buttons

---
+++ hackTues/Assets/UI Scirpts/ResearchButtonHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResearchButtonHandler : MonoBehaviour
+{
+    public void ReaserchedButtonPressed(string name)
+    {
+    }
+}

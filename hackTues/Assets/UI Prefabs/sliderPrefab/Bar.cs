98daf961bf0e980858770e33649be666f10600cb
Martin Ganev <marty.ganev@gmail.com>
2019-03-16 17:08:30 +0200

    Adding slider prefab

---
+++ hackTues/Assets/UI Prefabs/sliderPrefab/Bar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    public int p = 100;
+    void Update()
+    {
+        SetBarPercent(p);
+    }
+
+    void SetBarPercent(float percent)
+    {
+        if(percent > 100)
+        {
+            percent = 100;
+        }
+        else if(percent < 0)
+        {
+            percent = 0;
+        }
+        Transform b = transform.Find("BarSetup").transform;
+        b.localScale = new Vector3(percent / 100, b.localScale.y, b.localScale.z);
+    }
+}
